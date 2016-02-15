e549ba507c690f6604eff5e3a77efd3cb873bc99
Mudit Jain <1996mudit1502@gmail.com>
2016-02-16 02:36:39 +0530

    some-media-queries

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 /* anchor smooth scroll page */
+
+/****
+
+
+
 $(function () {
     $.srSmoothscroll()
 });
 /* anchor smooth scroll */
+
+/*****
+
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -15,5 +23,24 @@ $(function() {
         return false;
       }
     }
+  });
+});
+
+
+
+***/
+
+
+/*mobo nav*/
+
+$(function(){
+  $('.nav-trigger').on('click', function(){
+    $(this).toggleClass('open').toggleClass('fixme');
+    $('.mobo-nav').toggleClass('show-me');
+    });
+  $('.mobo-nav nav ul li a').click(function(){
+    $('.nav-trigger').removeClass('open').removeClass('fixme');
+    $('.mobo-nav').removeClass('show-me');
+
   });
 });
