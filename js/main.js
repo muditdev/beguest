d6ea9c10fc2f9f7a650e768c2f44dc52b3c7c21d
Mudit Jain <1996mudit1502@gmail.com>
2016-02-16 09:10:39 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,67 @@
+new WOW().init();
+
+/* anchor smooth scroll page */
+
+
+
+
+
+$(function () {
+    $.srSmoothscroll()
+});
+/* anchor smooth scroll */
+
+
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+
+
+$(window).scroll(function(){
+  var wScroll = $(this).scrollTop();
+  //wScroll = wScroll/3;
+  console.log(wScroll);
+
+
+
+
+
+
+});
+
+/*mobo nav*/
+
+$(function(){
+  $('.nav-trigger').on('click', function(){
+    $(this).toggleClass('open').toggleClass('fixme');
+    $('.mobo-nav').toggleClass('show-me');
+    });
+  $('.mobo-nav nav ul li a').click(function(){
+    $('.nav-trigger').removeClass('open').removeClass('fixme');
+    $('.mobo-nav').removeClass('show-me');
+
+
+  });
+
+
+
+
+
+
+
+});
